267af731c937741d4fa93d46cefd2a057f7b9c6c
Diego Muñoz <dmunozs86@gmail.com>
2015-08-13 14:26:11 -0300

    modified fabfile to work

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
 import os
 import time
 from fabric.api import env, require, run, sudo, cd, local, get
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+file_name = 'videoclases/project/settings_secret.py'
+template_name = 'videoclases/project/settings_secret.py.template'
 
 def _load_data(reboot=False):
     local('python manage.py makemigrations')
@@ -25,13 +28,14 @@ def reboot():
     restart(True)
 
 def install():
-    local('cp videoclases/project/settings_secret.py.template videoclases/project/settings_secret.py')
+    local('cp ' + os.path.join(BASE_DIR, template_name) + ' ' + os.path.join(BASE_DIR, file_name))
     _load_data()
     local('python manage test')
     local('python manage.py runserver 0.0.0.0:8000')
 
 def install_with_data():
-    local('cp videoclases/project/settings_secret.py.template videoclases/project/settings_secret.py')
+    local('cp ' + os.path.join(BASE_DIR, template_name) + ' ' + os.path.join(BASE_DIR, file_name))
     _load_data(True)
-    local('python manage test')
+    local('python manage.py collectstatic --noinput -l')
+    local('python manage.py test')
     local('python manage.py runserver 0.0.0.0:8000')
