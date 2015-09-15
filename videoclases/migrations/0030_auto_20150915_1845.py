aa26eba4bf409f646f705bcb469428ef2ec815fc
Diego Muñoz <dmunozs86@gmail.com>
2015-09-15 18:49:14 -0300

    changed profesor table to show the assigned profesor to the tarea

---
+++ videoclases/migrations/0030_auto_20150915_1845.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('videoclases', '0029_load_initial_data'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='booleanparameters',
+            options={'verbose_name': 'Mis Par\xe1metros', 'verbose_name_plural': 'Mis Par\xe1metros'},
+        ),
+    ]
