2da671a42ca200d19f52e961ffa7e5bde177afa1
jampueroc <jorge.ampuero.1a@gmail.com>
2016-06-02 23:39:31 -0400

    missing import

--- videoclases/views.py
+++ videoclases/views.py
@@ -26,6 +26,7 @@ from pyexcel_ods import get_data as ods_get_data
 from pyexcel_xls import get_data as xls_get_data
 from pyexcel_xlsx import get_data as xlsx_get_data
 from videoclases.forms import *
+from videoclases.models.boolean_parameters import BooleanParameters
 from videoclases.models.groupofstudents import GroupOfStudents
 from videoclases.models.school import School
 from videoclases.models.student import Student
