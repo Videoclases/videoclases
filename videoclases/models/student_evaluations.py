4115eac42171728a8ff32e2e4e413aebc6a09b50
Jorge Ampuero <jorge.ampuero.1a@gmail.com>
2017-11-27 01:54:38 -0300

    Updating deprecated model to use 5 options to evaluate and adding function to get criterias (old and new model)

--- videoclases/models/student_evaluations.py
+++ videoclases/models/student_evaluations.py
@@ -16,9 +16,9 @@ class StudentEvaluations(models.Model):
     # DEPRECATED
     scores = [
         (u'No cumple el criterio', 0),
-        # (u'Cumple muy parcialmente el criterio', 0.3),
+        (u'Cumple muy parcialmente el criterio', 0.3),
         (u'Cumple parcialmente el criterio', 0.5),
-        # (u'Cumple en su gran mayoria el criterio', 0.8),
+        (u'Cumple en su gran mayoria el criterio', 0.8),
         (u'Cumple el criterio', 1)
     ]
 
@@ -42,3 +42,44 @@ class StudentEvaluations(models.Model):
             self.videoclase.id,
             self.videoclase.homework.full_name()
         )
+
+    def get_evaluation(self):
+        if self.criterias.count() > 0:
+            result = list()
+            for c in self.criterias.select_related('criteria'):
+                result.append(
+                    {'id': c.id, 'name': c.criteria.value, 'value': c.value}
+                )
+            return result
+        return [
+            {
+                'id': 'originality',
+                'name': 'Originalidad',
+                'value': self.originality
+            },
+            {
+              'id' : 'format',
+                'name':'Formato',
+                'value':self.format
+            },
+            {
+              'id' : 'copyright',
+                'name':'Licencias',
+                'value':self.copyright
+            },
+            {
+              'id' : 'theme',
+                'name':'Tema',
+                'value':self.theme
+            },
+            {
+              'id' : 'pedagogical',
+                'name': 'Pedagogía',
+                'value': self.pedagogical
+            },
+            {
+              'id' : 'rythm',
+                'name':'Ritmo',
+                'value':self.rythm
+            },
+        ]
