60d2394a5566f145e9d1db99cc24e206086691fd
Jorge Ampuero <jorge.ampuero.1a@gmail.com>
2017-12-08 16:18:26 -0300

    Adding related name to find more easy relation

--- quality_control/models/quality_item.py
+++ quality_control/models/quality_item.py
@@ -2,15 +2,25 @@
 from django.db import models
 
 from videoclases.models.teacher import Teacher
-from .quality_score import QualityScore
 from videoclases.models.video_clase import VideoClase
+from .quality_score import QualityScore
 
 
 class QualityItem(models.Model):
-    videoclase = models.ForeignKey(VideoClase)
+    videoclase = models.ForeignKey(VideoClase, related_name='qualityItemList')
     score_check = models.ManyToManyField(QualityScore)
     teacher = models.ForeignKey(Teacher)
     comments = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return u"{0}".format(self.videoclase.question[:50])
+
+    def get_evaluation(self):
+        if self.score_check.count() > 0:
+            result = list()
+            for c in self.score_check.select_related('criteria'):
+                result.append(
+                    {'id': c.criteria.id, 'name': c.criteria.value, 'value': c.score}
+                )
+            return result
+        return []
